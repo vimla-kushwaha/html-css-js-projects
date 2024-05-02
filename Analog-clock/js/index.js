8b3406f12c3eeef167063edbcc4f3d1191ad8a84
vimla-kushwaha <vimlakushwaha01@gmail.com>
2024-05-02 17:33:42 +0530

    working analgo clock

---
+++ Analog-clock/js/index.js
@@ -0,0 +1,10 @@
+let hr = document.getElementById("hour");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+function displayTime(){
+    let date = new Date();
+
+    
+}
+setInterval(displayTime,1000);
