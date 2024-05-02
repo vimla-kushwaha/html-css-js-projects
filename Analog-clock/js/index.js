189780e3531fb2b90682d28733c5075379bbe4bd
vimla-kushwaha <vimlakushwaha01@gmail.com>
2024-05-02 19:21:10 +0530

    done second project[day2]

--- Analog-clock/js/index.js
+++ Analog-clock/js/index.js
@@ -1,21 +1,21 @@
-let hr = document.getElementById("hour");
+let hr = document.getElementById('hour');
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
 function displayTime(){
     let date = new Date();
-    let h = date.getHours();
-    let m = date.getMinutes();
-    let s = date.getSeconds();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
 
-    let hour_rotation = 30*h + m/2;
-    let minutes_rotation = 6*m;
-    let second_rotation = 6*s;
+    let hour_rotation = 30*hh + mm/2;
+    let minutes_rotation = 6*mm;
+    let second_rotation = 6*ss;
     
 
     hr.style.transform=  `rotate(${hour_rotation}deg)`;
-    m.style.transform=  `rotate(${minutes_rotation}deg)`;
-    s.style.transform=  `rotate(${second_rotation}deg)`;
+    min.style.transform=  `rotate(${minutes_rotation}deg)`;
+    sec.style.transform=  `rotate(${second_rotation}deg)`;
 
 }
 setInterval(displayTime,1000);
