13189b35b8c9c9421319ac5041dc5e48cf2cb325
vimla-kushwaha <vimlakushwaha01@gmail.com>
2024-05-03 22:47:00 +0530

    third project[03-may-24]

---
+++ project-3[analog-watch-2]/js/index.js
@@ -0,0 +1,9 @@
+const hrEle = document.querySelector(".hr");
+const minEle = document.querySelector(".min");
+const secEle = document.querySelector(".sec");
+
+setInterval(()=>{
+    let date = new Date();
+    let hr = date.getHours();
+    
+})
